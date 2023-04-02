28330806ef85940ecf2b648ea21af0d9731702f8
SNAKERIX <snake1123riking@gmail.com>
2023-04-03 00:07:15 +0900

    first commit

---
+++ Assets/Data/blockdata.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Block/BlockData")]
+public class blockdata : ScriptableObject
+{
+    [Range(1,5)]
+    public int Grade = 1;
+    public int cost;
+    public float HP;
+    public Sprite Icon;
+    public bool CanTdestoroy=false;
+    [System.Serializable]
+    public struct LevelUp
+    {
+        public string name;
+        public GameObject Nextobj;
+    }
+    public List<LevelUp> levelUps;
+    public GameObject levelEffect;
+    public enum BlockType
+    {
+        Wall,
+        Canon,
+        Attacker,
+        Unique,
+    }
+    public BlockType blockType;
+    public enum AttackType
+    {
+        None,
+        Stab,
+        Slash,
+        Shoot,
+    }       
+    public AttackType attackType; 
+    public float Damage;
+    public int penetration=0;
+    public Vector3 Knockback;
+    public float Range;
+    public float RangeRate;
+    public float AttackRate;
+    public GameObject Bullet;
+    public GameObject Shoteffect;
+    public float effectsize = 0.7f;
+    public float BulletSpeed;
+    public string explain;
+    public GameObject HitEffect;
+    public float effectSize = 1.2f;
+
+}
---
+++ Assets/Data/enemydata.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Enemytype")]
+public class enemydata : ScriptableObject
+{
+    public int cost;
+    public float HP;
+    [Range(1,3)]
+    public int hight=1;
+    public float Damage;
+    public bool norange;
+    public float Range;
+    public float AtackeRate;
+    public GameObject Shoteffect;
+    public float Shotsize;
+    public GameObject Bullet;
+    public float Speed;
+    public Sprite Icon;
+    public GameObject HitEffect;
+    public float effectSize=1.2f;
+}
